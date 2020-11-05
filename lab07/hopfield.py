7a475e20f068e0cabf73f9ca36f6ccda22ea5aab
Matii96 <mateusz.fonfara96@gmail.com>
2020-11-05 16:36:36 +0100

    Added some comments

--- lab07/hopfield.py
+++ lab07/hopfield.py
@@ -10,9 +10,13 @@ def Train(patterns):
   firstPattern = np.array(patterns[0])
   pixelsCount = firstPattern.shape[0] * firstPattern.shape[1]
 
+  # Convert arrays to vectors
   patternsConverted = np.array([ConvertInputArray(np.array(pattern), pixelsCount) for pattern in patterns])
 
+  # Initial weights matrix
   weights = np.zeros((pixelsCount, pixelsCount))
+
+  # Correct weights
   for i in range(pixelsCount):
     for j in range(pixelsCount):
       if i != j:
@@ -33,7 +37,7 @@ def Correct(weights, array, iterations=5):
     for i in range(pixelsCount):
       change[i] = 0
       for j in range(pixelsCount):
-        change[i] += weights[i, j] * correctedArray[j]
+        change[i] += correctedArray[j] * weights[i, j]
     correctedArray = np.where(change<0, -1, 1)
 
   correctedArray = np.where(correctedArray==-1, 0, 1).reshape(array.shape[0], array.shape[1])
