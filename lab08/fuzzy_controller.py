82bae74d3bd453cccf849d5bc797137516489a0e
Matii96 <mateusz.fonfara96@gmail.com>
2020-11-11 14:55:16 +0100

    code cleanup

--- lab08/fuzzy_controller.py
+++ lab08/fuzzy_controller.py
@@ -15,12 +15,4 @@ def fuzzy_controller(x, y, angle):
     raise Exception('Combination {}, {}, {} doesn\'t belong to any rule'.format(x, y, angle))
 
   # Make sure we're in [-180, 180] degrees range
-  new_value = angle + next_angle
-  if new_value > 180:
-    # next_angle -= new_value - 180
-    next_angle = 180
-  elif new_value < -180:
-    # next_angle -= new_value + 180
-    next_angle = -180
-
-  return next_angle
+  return max(min(next_angle, 180), -180)
